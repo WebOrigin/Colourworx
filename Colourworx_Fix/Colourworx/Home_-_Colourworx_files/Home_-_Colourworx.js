79a54f8a9b08c8338cb0cd93808e2ff99c5856c8
Leask Huang <i@leaskh.com>
2012-05-23 22:05:56 +0800

    adding page source and final output

---
+++ Colourworx_Fix/Colourworx/Home_-_Colourworx_files/Home_-_Colourworx.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.3 local-build-20080319
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Home_-_Colourworx_files/Home_-_ColourworxMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Colourworx_Fix/Colourworx/files_files/files.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.3 local-build-20080319
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,0.0000),color:'#ffffff',opacity:0.050000}),stroke_0:new IWEmptyStroke(),shadow_1:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,0.0000),color:'#ffffff',opacity:0.050000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('files_files/filesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
